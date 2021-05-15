44a1057d81ba69c6a2b079a93428a7ea1aeedaa3
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-15 23:42:16 +0100

    Created custom exceptions

---
+++ src/users-logic/Exceptions/Command/QueryRequestException.cs
@@ -0,0 +1,20 @@
+namespace users_logic.Exceptions.Command
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    public class QueryRequestException : Exception
+    {
+        public QueryRequestException(string message) : base(message)
+        {
+        }
+
+        public QueryRequestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected QueryRequestException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/users-logic/Exceptions/User/UserNotFoundException.cs
@@ -0,0 +1,24 @@
+namespace users_logic.Exceptions.User
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException()
+        {
+        }
+
+        public UserNotFoundException(string message) : base(message)
+        {
+        }
+
+        public UserNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UserNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
