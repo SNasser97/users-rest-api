33e13067171c10bf344fbf14bdf893926c9a437f
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-16 11:41:03 +0100

    Removed unused using statement

--- src/users-logic/User/Logic/Query/IUserQuery.cs
+++ src/users-logic/User/Logic/Query/IUserQuery.cs
@@ -1,6 +1,5 @@
 namespace users_logic.User.Logic.Query
 {
-    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public interface IUserQuery<TRequest, TResponse, TResponses>
