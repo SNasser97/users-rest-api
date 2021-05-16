7e75c108d5cdfbe393568b76ea8b32a7bd9ae6aa
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-16 11:04:25 +0100

    Created GetUsersResponseModel and changed interface

--- src/users-logic/User/Logic/Query/IUserQuery.cs
+++ src/users-logic/User/Logic/Query/IUserQuery.cs
@@ -3,10 +3,10 @@ namespace users_logic.User.Logic.Query
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
-    public interface IUserQuery<TRequest, TResponse>
+    public interface IUserQuery<TRequest, TResponse, TResponses>
     {
         Task<TResponse> GetReponseAsync(TRequest request);
 
-        Task<IEnumerable<TResponse>> GetReponsesAsync();
+        Task<TResponses> GetReponsesAsync();
     }
 }
---
+++ src/users-logic/User/Logic/Query/Models/Response/GetUsersResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace users_logic.User.Logic.Query.Models.Response
+{
+    using System.Collections.Generic;
+
+    public class GetUsersResponseModel
+    {
+        public IEnumerable<GetUserResponseModel> Users { get; set; }
+    }
+}
