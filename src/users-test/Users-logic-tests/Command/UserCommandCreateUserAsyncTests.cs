b303e1b233198be5d41e1525a27c58ec34127fc7
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-16 14:31:09 +0100

    Created test for email exception

--- src/users-test/Users-logic-tests/Command/UserCommandCreateUserAsyncTests.cs
+++ src/users-test/Users-logic-tests/Command/UserCommandCreateUserAsyncTests.cs
@@ -11,6 +11,7 @@ namespace users_test.Users_logic_tests.Command
     using users_logic.User.Logic.Command.Models.Request;
     using users_logic.User.Logic.Command.Models.Response;
     using System.Collections.Generic;
+    using users_logic.Exceptions.Command;
 
     public class UserCommandCreateUserAsyncTests
     {
@@ -69,6 +70,42 @@ namespace users_test.Users_logic_tests.Command
             mockUserLogicFacade.Verify(s => s.GetCalculatedUsersAgeAsync(It.IsAny<DateTime>()), Times.Once);
             mockUserLogicFacade.Verify(s => s.IsAgeValidAsync(It.IsAny<int>()), Times.Once);
             mockUserWriteRepository.Verify(s => s.CreateAsync(It.IsAny<CreateUserRecord>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task UserCommand_CreateUserAsync_TakesExistingEmailInCreateUserCommandRequest_ExpectsCommandRequestException()
+        {
+            //Given
+
+            var createUserCommandRequest = new CreateUserCommandRequest
+            {
+                FirstName = "Bob",
+                LastName = "Doe",
+                Email = "b.doe@mail.com",
+                DateOfBirth = new DateTime(1992, 6, 12)
+            };
+
+            var mockUserWriteRepository = new Mock<IWriteRepository<BaseUserRecord, BaseUserRecordWithId>>();
+            var mockUserReadRepository = new Mock<IReadRepository<BaseUserRecordWithId>>();
+            var mockUserLogicFacade = new Mock<IUserLogicFacade>();
+            var userCommand = new UserCommand(mockUserWriteRepository.Object, mockUserReadRepository.Object, mockUserLogicFacade.Object);
+
+            mockUserReadRepository.Setup(s => s.GetAsync()).ReturnsAsync(It.IsAny<IEnumerable<UserRecord>>());
+            mockUserLogicFacade.Setup(s => s.DoesUserEmailAlreadyExistAsync(It.IsAny<IEnumerable<UserRecord>>(), createUserCommandRequest.Email))
+                .ReturnsAsync(true);
+
+            //When
+            //Then
+            await Exceptions<CommandRequestException>.HandleAsync(async ()
+                => await userCommand.CreateUserAsync(createUserCommandRequest),
+                (ex) => Assert.Equal("Email already exists", ex.Message)
+            );
+
+            mockUserReadRepository.Verify(s => s.GetAsync(), Times.Once);
+            mockUserLogicFacade.Verify(s => s.DoesUserEmailAlreadyExistAsync(It.IsAny<IEnumerable<UserRecord>>(), It.IsAny<string>()), Times.Once);
+            mockUserLogicFacade.Verify(s => s.GetCalculatedUsersAgeAsync(It.IsAny<DateTime>()), Times.Never);
+            mockUserLogicFacade.Verify(s => s.IsAgeValidAsync(It.IsAny<int>()), Times.Never);
+            mockUserWriteRepository.Verify(s => s.CreateAsync(It.IsAny<CreateUserRecord>()), Times.Never);
         }
     }
 }
