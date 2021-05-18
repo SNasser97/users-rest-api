35f8e51e415e80217fd275b6c4e56b334ea7ee3a
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-18 07:28:19 +0100

    Added  dependencies to service collection

--- src/users-api/Startup.cs
+++ src/users-api/Startup.cs
@@ -1,8 +1,21 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using users_data.Entities;
+using users_data.Repositories;
+using users_data.Repositories.InMemoryUserRepository;
+using users_logic.User.Facades;
+using users_logic.User.Logic.Command;
+using users_logic.User.Logic.Command.Models.Response;
+using users_logic.User.Logic.Query;
+using users_logic.User.Logic.Query.Models.Request;
+using users_logic.User.Logic.Query.Models.Response;
+using users_logic.User.Parser;
+using users_logic.User.Provider;
 
 namespace users_api
 {
@@ -18,6 +31,14 @@ namespace users_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IUserQuery<GetUserQueryRequestModel, GetUserQueryResponseModel, GetUsersQueryResponseModel>, UserQuery>();
+            services.AddScoped<IUserCommand<BaseUserCommandResponse>, UserCommand>();
+            services.AddScoped<IDateTimeParser, DateTimeParser>();
+            services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+            services.AddScoped<IUserLogicFacade, UserLogicFacade>();
+            services.AddSingleton<InMemoryUsersRepository>();
+            services.AddSingleton<IWriteRepository<BaseUserRecord, BaseUserRecordWithId>>(x => x.GetRequiredService<InMemoryUsersRepository>());
+            services.AddSingleton<IReadRepository<BaseUserRecordWithId>>(x => x.GetRequiredService<InMemoryUsersRepository>());
             services.AddControllers();
         }
 
