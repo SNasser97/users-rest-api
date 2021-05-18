f6674366a075abae948dbaf9bc932d8937241c6f
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-18 07:34:00 +0100

    Added try catch to POST

--- src/users-api/UserControllers/CommandControllers/UserCommandController.cs
+++ src/users-api/UserControllers/CommandControllers/UserCommandController.cs
@@ -1,8 +1,10 @@
 namespace users_api.UserControllers.CommandControllers
 {
+    using System;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using users_api.UserControllers.CommandControllers.Models.Request;
+    using users_logic.Exceptions.Validation;
     using users_logic.User.Logic.Command;
     using users_logic.User.Logic.Command.Models.Response;
 
@@ -20,8 +22,20 @@ namespace users_api.UserControllers.CommandControllers
         [HttpPost("users")]
         public async Task<IActionResult> Create([FromBody] CreateUserControllerRequestModel request)
         {
-            CreateUserCommandResponse a = (CreateUserCommandResponse)await this.userCommand.CreateUserAsync(request.ToCommandRequest());
-            return this.Ok(a.Id);
+            try
+            {
+                CreateUserCommandResponse a = (CreateUserCommandResponse)await this.userCommand.CreateUserAsync(request.ToCommandRequest());
+                return this.Ok(a.Id);
+
+            }
+            catch (EmailExistsException ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
         }
     }
 
