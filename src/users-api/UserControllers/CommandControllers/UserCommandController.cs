91cc5757a8e865c5a5416b495c1adef3c8bd7f5c
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-18 07:23:50 +0100

    Created post controller

---
+++ src/users-api/UserControllers/CommandControllers/UserCommandController.cs
@@ -0,0 +1,29 @@
+namespace users_api.UserControllers.CommandControllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using users_api.UserControllers.CommandControllers.Models.Request;
+    using users_logic.User.Logic.Command;
+    using users_logic.User.Logic.Command.Models.Response;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserCommandController : ControllerBase
+    {
+        private readonly IUserCommand<BaseUserCommandResponse> userCommand;
+
+        public UserCommandController(IUserCommand<BaseUserCommandResponse> userCommand)
+        {
+            this.userCommand = userCommand;
+        }
+
+        [HttpPost("users")]
+        public async Task<IActionResult> Create([FromBody] CreateUserControllerRequestModel request)
+        {
+            CreateUserCommandResponse a = (CreateUserCommandResponse)await this.userCommand.CreateUserAsync(request.ToCommandRequest());
+            return this.Ok(a.Id);
+        }
+    }
+
+
+}
