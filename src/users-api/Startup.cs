174551e9b9ec5368debbf4f24ebcba05dd482278
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-18 10:31:30 +0100

    Created method for user dependencies

--- src/users-api/Startup.cs
+++ src/users-api/Startup.cs
@@ -1,24 +1,22 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using users_data.Entities;
-using users_data.Repositories;
-using users_data.Repositories.InMemoryUserRepository;
-using users_logic.User.Facades;
-using users_logic.User.Logic.Command;
-using users_logic.User.Logic.Command.Models.Response;
-using users_logic.User.Logic.Query;
-using users_logic.User.Logic.Query.Models.Request;
-using users_logic.User.Logic.Query.Models.Response;
-using users_logic.User.Parser;
-using users_logic.User.Provider;
-
 namespace users_api
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using users_data.Entities;
+    using users_data.Repositories;
+    using users_data.Repositories.InMemoryUserRepository;
+    using users_logic.User.Facades;
+    using users_logic.User.Logic.Command;
+    using users_logic.User.Logic.Command.Models.Response;
+    using users_logic.User.Logic.Query;
+    using users_logic.User.Logic.Query.Models.Request;
+    using users_logic.User.Logic.Query.Models.Response;
+    using users_logic.User.Parser;
+    using users_logic.User.Provider;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -31,17 +29,9 @@ namespace users_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IUserQuery<GetUserQueryRequestModel, GetUserQueryResponseModel, GetUsersQueryResponseModel>, UserQuery>();
-            services.AddScoped<IUserCommand<BaseUserCommandResponse>, UserCommand>();
-            services.AddScoped<IDateTimeParser, DateTimeParser>();
-            services.AddScoped<IDateTimeProvider, DateTimeProvider>();
-            services.AddScoped<IUserLogicFacade, UserLogicFacade>();
-            services.AddSingleton<InMemoryUsersRepository>();
-            services.AddSingleton<IWriteRepository<BaseUserRecord, BaseUserRecordWithId>>(x => x.GetRequiredService<InMemoryUsersRepository>());
-            services.AddSingleton<IReadRepository<BaseUserRecordWithId>>(x => x.GetRequiredService<InMemoryUsersRepository>());
+            this.AddUserDependencies(services);
             services.AddControllers();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -60,6 +50,19 @@ namespace users_api
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void AddUserDependencies(IServiceCollection services)
+        {
+            // Where IRecordData contains user data which user write + read access
+            services.AddSingleton<IRecordData<BaseUserRecordWithId>, InMemoryUsersRecordData>();
+            services.AddScoped<IWriteRepository<BaseUserRecord, BaseUserRecordWithId>, InMemoryUserWriteRepository>();
+            services.AddScoped<IReadRepository<BaseUserRecordWithId>, InMemoryUserReadRepository>();
+            services.AddScoped<IUserQuery<GetUserQueryRequestModel, GetUserQueryResponseModel, GetUsersQueryResponseModel>, UserQuery>();
+            services.AddScoped<IUserCommand<BaseUserCommandResponse>, UserCommand>();
+            services.AddScoped<IDateTimeParser, DateTimeParser>();
+            services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+            services.AddScoped<IUserLogicFacade, UserLogicFacade>();
         }
     }
 }
