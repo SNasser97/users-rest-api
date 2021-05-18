f12497a67a42404e5adfc2d8a3b2670ea55ee8bb
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-18 07:24:20 +0100

    Created controller response model

---
+++ src/users-api/UserControllers/QueryControllers/Models/Response/GetUserControllerResponsesModel.cs
@@ -0,0 +1,9 @@
+namespace users_api.UserControllers.QueryControllers.Models.Response
+{
+    using users_logic.User.Logic.Query.Models.Response;
+
+    public class GetUserControllerResponsesModel
+    {
+        public GetUsersQueryResponseModel Responses { get; set; }
+    }
+}
