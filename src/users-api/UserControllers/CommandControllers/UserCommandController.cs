68ae55704f79ac32b50d8b526801af1035de91d8
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-19 14:54:51 +0100

    Created command controllers

--- src/users-api/UserControllers/CommandControllers/UserCommandController.cs
+++ src/users-api/UserControllers/CommandControllers/UserCommandController.cs
@@ -3,28 +3,30 @@ namespace users_api.UserControllers.CommandControllers
     using System;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using users_api.Extensions;
     using users_api.UserControllers.CommandControllers.Models.Request;
     using users_logic.Exceptions.Validation;
     using users_logic.User.Logic.Command;
+    using users_logic.User.Logic.Command.Models.Request;
     using users_logic.User.Logic.Command.Models.Response;
 
     [ApiController]
-    [Route("[controller]")]
+    [Route("users")]
     public class UserCommandController : ControllerBase
     {
-        private readonly IUserCommand<BaseUserCommandResponse> userCommand;
+        private readonly IUserCommand<BaseUserCommandResponseModel> userCommand;
 
-        public UserCommandController(IUserCommand<BaseUserCommandResponse> userCommand)
+        public UserCommandController(IUserCommand<BaseUserCommandResponseModel> userCommand)
         {
-            this.userCommand = userCommand;
+            this.userCommand = userCommand ?? throw new ArgumentNullException(nameof(userCommand));
         }
 
-        [HttpPost("users")]
-        public async Task<IActionResult> Create([FromBody] CreateUserControllerRequestModel request)
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateUserControllerRequestModel request)
         {
             try
             {
-                CreateUserCommandResponse a = (CreateUserCommandResponse)await this.userCommand.CreateUserAsync(request.ToCommandRequest());
+                CreateUserCommandResponseModel a = (CreateUserCommandResponseModel)await this.userCommand.CreateUserAsync(request.ToUserCommandRequest());
                 return this.Ok(a.Id);
 
             }
@@ -37,7 +39,19 @@ namespace users_api.UserControllers.CommandControllers
                 return this.BadRequest(ex.Message);
             }
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync([FromRoute] Guid id, UpdateUserControllerRequestModel request)
+        {
+            UpdateUserCommandResponseModel a = (UpdateUserCommandResponseModel)await this.userCommand.UpdateUserAsync(request.ToUserCommandRequest(id));
+            return this.Ok(a.Id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] Guid id)
+        {
+            await this.userCommand.DeleteUserAsync(new DeleteUserCommandRequestModel { Id = id });
+            return this.Ok();
+        }
     }
-
-
 }
