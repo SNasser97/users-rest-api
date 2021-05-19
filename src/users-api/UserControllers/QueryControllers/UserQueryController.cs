949edd04fe1a6379b65720b42ce329a37b36f345
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-19 14:56:12 +0100

    Created query controllers

--- src/users-api/UserControllers/QueryControllers/UserQueryController.cs
+++ src/users-api/UserControllers/QueryControllers/UserQueryController.cs
@@ -1,6 +1,6 @@
 namespace users_api.UserControllers.QueryControllers
 {
-    using System.Collections.Generic;
+    using System;
     using System.Diagnostics;
     using System.Text.Json;
     using System.Threading.Tasks;
@@ -10,7 +10,7 @@ namespace users_api.UserControllers.QueryControllers
     using users_logic.User.Logic.Query.Models.Response;
 
     [ApiController]
-    [Route("[controller]")]
+    [Route("users")]
     public class UserQueryController : ControllerBase
     {
         private readonly IUserQuery<GetUserQueryRequestModel, GetUserQueryResponseModel, GetUsersQueryResponseModel> userQuery;
@@ -20,12 +20,19 @@ namespace users_api.UserControllers.QueryControllers
             this.userQuery = userQuery ?? throw new System.ArgumentNullException(nameof(userQuery));
         }
 
-        [HttpGet("users")]
+        [HttpGet]
         public async Task<GetUsersQueryResponseModel> GetAsync()
         {
             var a = await this.userQuery.GetResponsesAsync();
             Debug.WriteLine(JsonSerializer.Serialize(a.Users));
             return a;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync([FromRoute] Guid id)
+        {
+            var b = await this.userQuery.GetReponseAsync(new GetUserQueryRequestModel { Id = id });
+            return this.Ok(b);
         }
     }
 }
