fb793035e17509d0439c13e4a52da6b2c03ab9d0
Nasser Ahmed <snasserahmed@outlook.com>
2021-05-22 19:14:39 +0100

    Removed comments

--- src/users-data/Repositories/IWriteRepository.cs
+++ src/users-data/Repositories/IWriteRepository.cs
@@ -5,13 +5,10 @@
 
     public interface IWriteRepository<TRecord, T2Record>
     {
-        // Create TRecord
         Task<Guid> CreateAsync(TRecord record);
 
-        // Updata TRecord
         Task<Guid> UpdateAsync(T2Record record);
 
-        // Delete TRecord
         Task DeleteAsync(Guid id);
     }
 }
