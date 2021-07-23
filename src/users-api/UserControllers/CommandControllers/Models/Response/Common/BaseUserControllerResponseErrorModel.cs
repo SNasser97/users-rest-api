025f28f705cb616134c262f5998bdbc781b73519
Nasser Ahmed <snasserahmed@outlook.com>
2021-07-23 23:20:49 +0100

    Removed nullable as string can be null

--- src/users-api/UserControllers/CommandControllers/Models/Response/Common/BaseUserControllerResponseErrorModel.cs
+++ src/users-api/UserControllers/CommandControllers/Models/Response/Common/BaseUserControllerResponseErrorModel.cs
@@ -2,6 +2,6 @@ namespace users_api.UserControllers.CommandControllers.Models.Response.Common
 {
     public abstract class BaseUserControllerResponseErrorModel
     {
-        public string? Error { get; set; }
+        public string Error { get; set; }
     }
 }
