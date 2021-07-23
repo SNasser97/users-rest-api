8c82a3795e05387fc92020c2b931e189df12144d
Nasser Ahmed <snasserahmed@outlook.com>
2021-07-23 21:48:00 +0100

    Changed to use concurrent bag

--- src/users-logic/User/Logic/Query/Models/Response/GetUsersQueryResponseModel.cs
+++ src/users-logic/User/Logic/Query/Models/Response/GetUsersQueryResponseModel.cs
@@ -1,12 +1,13 @@
 ﻿namespace users_logic.User.Logic.Query.Models.Response
 {
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
 
     public class GetUsersQueryResponseModel
     {
         public IEnumerable<GetUserQueryResponseModel> Users { get => this.users; }
 
-        private readonly IList<GetUserQueryResponseModel> users = new List<GetUserQueryResponseModel>();
+        private readonly ConcurrentBag<GetUserQueryResponseModel> users = new ConcurrentBag<GetUserQueryResponseModel>();
 
         public void AddResponse(GetUserQueryResponseModel model)
         {
