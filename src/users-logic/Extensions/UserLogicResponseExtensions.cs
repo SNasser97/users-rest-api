1e1dbd728c4c007ad967766dfe112691b4a1fba5
Nasser Ahmed <snasserahmed@outlook.com>
2021-07-28 18:40:46 +0100

    Removed unused ext method

--- src/users-logic/Extensions/UserLogicResponseExtensions.cs
+++ src/users-logic/Extensions/UserLogicResponseExtensions.cs
@@ -1,12 +1,10 @@
 namespace users_logic.Extensions
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using users_data.Entities;
     using users_logic.Logic.Query;
-    using users_logic.Logic.Query.Models.Response;
 
     public static class UserLogicResponseExtensions
     {
@@ -26,13 +24,5 @@ namespace users_logic.Extensions
         public static async Task<IEnumerable<GetUserQueryResponse>> MapToGetUsersQueryResponseModel(this IEnumerable<User> source)
             => await (Task.WhenAll(source.Select(r
                 => Task.Run(() => r.ToResponseModel()))));
-
-        public static void ForEachResponse(this IEnumerable<GetUserQueryResponseModel> source, Action<GetUserQueryResponseModel> action)
-        {
-            foreach (GetUserQueryResponseModel model in source)
-            {
-                action(model);
-            }
-        }
     }
 }
