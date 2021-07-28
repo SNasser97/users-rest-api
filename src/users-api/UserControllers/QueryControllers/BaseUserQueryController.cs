d5d43b04c64a1aecdb1765887a8d6b02cc7d73fa
Nasser Ahmed <snasserahmed@outlook.com>
2021-07-28 19:27:00 +0100

    Cleaned up imports

--- src/users-api/UserControllers/QueryControllers/BaseUserQueryController.cs
+++ src/users-api/UserControllers/QueryControllers/BaseUserQueryController.cs
@@ -1,11 +1,6 @@
 namespace users_api.UserControllers.QueryControllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
-    using users_api.Extensions;
-    using users_api.UserControllers.QueryControllers.Models.Response;
     using users_logic.Logic.Query;
 
     [ApiController]
