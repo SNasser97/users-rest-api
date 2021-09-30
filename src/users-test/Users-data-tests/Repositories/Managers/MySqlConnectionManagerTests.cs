b9fe4e2d2ca7a1d26a56347089be120f68fb696d
Nasser Ahmed <snasserahmed@outlook.com>
2021-09-30 15:56:10 +0100

    Added skippable xunit tests on connection manager

--- src/users-test/Users-data-tests/Repositories/Managers/MySqlConnectionManagerTests.cs
+++ src/users-test/Users-data-tests/Repositories/Managers/MySqlConnectionManagerTests.cs
@@ -1,6 +1,8 @@
 namespace users_test.Users_data_tests.Repositories.Managers
 {
     using System;
+    using System.Data;
+    using System.Data.Common;
     using System.Threading.Tasks;
     using MySql.Data.MySqlClient;
     using users_data.Repositories.MySQL.MySqlManagers;
@@ -9,18 +11,21 @@ namespace users_test.Users_data_tests.Repositories.Managers
     public class MySqlConnectionManagerTests
     {
         // Requires a running mysql docker container for tests
-        private string connectionErrorMessage = "Access denied for user ''@'172.18.0.1' (using password: NO)";
+        private string connectionErrorMessage = "Access denied for user";
+        private string noMySqlConnectionMessage = "Unable to connect to any of the specified MySQL hosts.";
         private Lazy<string> connectionStringTestValue = new Lazy<string>(Environment.GetEnvironmentVariable("MYSQL_CONNECTION") ?? $"Server=localhost;Uid=admin;Pwd=secret;Database=users_db;");
 
-        [Fact]
+        [SkippableFact]
         public async Task MySqlConnectionManagerGetDbConnectionAsyncReturnsInvalidMySqlConnectionEntity()
         {
             // Given
+
             var connectionString = new Lazy<string>("");
             var connectionManager = new MySqlConnectionManager(connectionString);
 
             // When
             MySqlConnection actualConnection = (MySqlConnection)await connectionManager.GetDbConnectionAsync();
+            await this.SkipTestOnCondition<MySqlConnection, MySqlException>(actualConnection, noMySqlConnectionMessage);
 
             // Then
             Assert.True(string.IsNullOrWhiteSpace(actualConnection.Database));
@@ -33,10 +38,11 @@ namespace users_test.Users_data_tests.Repositories.Managers
                 {
                     await actualConnection.OpenAsync();
                 }
-            }, (ex) => Assert.True(ex.Message.Contains(connectionErrorMessage)));
+            }, (ex) => Assert.True(true == ex.Message.Contains(connectionErrorMessage)));
+            // Assert.True(ex.Message.Contains(connectionErrorMessage))
         }
 
-        [Fact]
+        [SkippableFact]
         public async Task MySqlConnectionManagerGetDbConnectionAsyncReturnsValidMySqlConnectionEntity()
         {
             // Given
@@ -44,6 +50,7 @@ namespace users_test.Users_data_tests.Repositories.Managers
 
             // When
             MySqlConnection actualConnection = (MySqlConnection)await connectionManager.GetDbConnectionAsync();
+            await this.SkipTestOnCondition<MySqlConnection, MySqlException>(actualConnection, noMySqlConnectionMessage);
 
             // Then
             Assert.True(!string.IsNullOrWhiteSpace(actualConnection.Database));
@@ -60,6 +67,32 @@ namespace users_test.Users_data_tests.Repositories.Managers
 
                 Assert.Equal("Closed", actualConnection.State.ToString());
             });
+        }
+
+        // TODO: Extract as helper methods
+        private async Task<bool> ExceptionMessageOn<TConnectionEntity, TException>(TConnectionEntity entity, string message)
+            where TConnectionEntity : DbConnection
+            where TException : Exception
+        {
+            try
+            {
+                await entity.OpenAsync();
+                await entity.CloseAsync();
+                return false;
+            }
+            catch (TException ex)
+            {
+                return ex.Message.Equals(message);
+            }
+        }
+
+        private async Task SkipTestOnCondition<TConnectionEntity, TException>(TConnectionEntity entity, string message)
+            where TConnectionEntity : DbConnection
+            where TException : Exception
+        {
+            // Ignore the above tests if we're building the dotnet api image
+            bool isDotNetImageBeingBuilt = await this.ExceptionMessageOn<TConnectionEntity, TException>(entity, message);
+            Skip.If(isDotNetImageBeingBuilt, "Only run on dev machine");
         }
     }
 }
