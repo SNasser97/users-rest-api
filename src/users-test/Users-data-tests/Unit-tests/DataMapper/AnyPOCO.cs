7452cb67dcf465330ffddf1c8bcd5cf1837ee25f
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2021-11-26 23:56:43 +0000

    Created AnyPOCO test class

---
+++ src/users-test/Users-data-tests/Unit-tests/DataMapper/AnyPOCO.cs
@@ -0,0 +1,19 @@
+namespace users_test.Users_data_tests.Unit_tests.DataMapper
+{
+    using System;
+
+    public class AnyPOCO
+    {
+        public string MyPropertyString { get; set; }
+
+        public int MyPropertyInt { get; set; }
+
+        public DateTime MyPropertyDateTime { get; set; }
+
+        public Guid MyPropertyGuid { get; set; }
+
+        public Guid MyPropertyGuid2 { get; set; }
+
+        public Guid MyPropertyGuid3 { get; set; }
+    }
+}
