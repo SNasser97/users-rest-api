e38e1acf92cc9f6101c0672afef03c7b7191ee5e
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2021-11-26 23:54:14 +0000

    Removed unused directive (System.Data)

--- src/users-data/Repositories/MySQL/Users/MySqlUserReadRepository.cs
+++ src/users-data/Repositories/MySQL/Users/MySqlUserReadRepository.cs
@@ -2,7 +2,6 @@ namespace users_data.Repositories.MySQL
 {
     using System;
     using System.Collections.Generic;
-    using System.Data;
     using System.Data.Common;
     using System.Linq;
     using System.Text.Json;
