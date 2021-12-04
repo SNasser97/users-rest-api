b9a6972f3215af91ecf9431df39aab2ce0e958ee
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2021-12-04 23:15:53 +0000

    Created tests for db transaction manager

---
+++ src/users-integration-test/Managers/MySqlTransactionManagerTests.cs
@@ -0,0 +1,51 @@
+namespace users_integration_test.Managers
+{
+    using System;
+    using System.Threading.Tasks;
+    using MySql.Data.MySqlClient;
+    using users_data.Repositories.MySQL.MySqlManagers;
+    using users_test.Helper;
+    using Xunit;
+
+    public class MySqlTransactionManagerTests
+    {
+        private Lazy<string> connectionStringTestValue = new Lazy<string>(Environment.GetEnvironmentVariable("MYSQL_CONNECTION") ?? $"Server=localhost;Uid=admin;Pwd=secret;Database=users_db;");
+
+        [Fact]
+        public async Task MySqlTransactionManagerExecutesTransactionExpectsReturnedValue()
+        {
+            var connectionManager = new MySqlConnectionManager(connectionStringTestValue);
+            var transactionManager = new MySqlTransactionManager(connectionManager);
+
+            Guid actualResult = await transactionManager.ExecuteTransactionAsync(async (transaction) =>
+            {
+                MySqlTransaction sqlTransaction = transaction as MySqlTransaction;
+                Assert.NotNull(sqlTransaction);
+                Assert.False(string.IsNullOrWhiteSpace(sqlTransaction.Connection.ConnectionString));
+
+                (string actualServer, string actualUid, string actualDb) = this.GetConnectionValues(sqlTransaction.Connection.ConnectionString.Split(';'));
+                Assert.True(actualServer.Contains("localhost"));
+                Assert.True(actualUid.Contains("admin"));
+                Assert.True(actualDb.Contains("users_db"));
+                return await Task.FromResult(Guid.NewGuid());
+            });
+
+            Assert.True(actualResult != Guid.Empty);
+        }
+
+        [Fact]
+        public async Task MySqlTransactionManagerExecutesTransactionThrowsMySqlException()
+        {
+            var connectionManager = new MySqlConnectionManager(new Lazy<string>(""));
+            var transactionManager = new MySqlTransactionManager(connectionManager);
+
+            await Exceptions<MySqlException>.HandleAsync(async () =>
+            {
+                await transactionManager.ExecuteTransactionAsync(async (transaction) => await Task.FromResult(Guid.NewGuid()));
+            }, (ex) => Assert.True(true == ex.Message.Contains("Access denied for user")));
+        }
+
+        private Tuple<string, string, string> GetConnectionValues(string[] values)
+            => new Tuple<string, string, string>(values[0], values[1], values[2]);
+    }
+}
