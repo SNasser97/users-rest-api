52ad67bc8be0f4721d2f2f3d677f8b21e5ccad37
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2021-12-04 23:14:55 +0000

    Created mysql db transaction manager class

--- src/users-data/Repositories/MySQL/MySqlManagers/MySqlTransactionManager.cs
+++ src/users-data/Repositories/MySQL/MySqlManagers/MySqlTransactionManager.cs
@@ -1,13 +1,41 @@
 namespace users_data.Repositories.MySQL.MySqlManagers
 {
+    using System;
+    using System.Data;
     using System.Threading.Tasks;
+    using MySql.Data.MySqlClient;
     using users_data.Manager;
 
     public class MySqlTransactionManager : IDbTransactionManager
     {
-        public Task ExecuteTransactionAsync()
+        private readonly IDbConnectionManager connectionManager;
+
+        public MySqlTransactionManager(IDbConnectionManager connectionManager)
         {
-            throw new System.NotImplementedException();
+            this.connectionManager = connectionManager;
+        }
+        public async Task<TValue> ExecuteTransactionAsync<TValue>(Func<IDbTransaction, Task<TValue>> transactionFunc)
+        {
+            using (MySqlConnection connection = await connectionManager.GetDbConnectionAsync() as MySqlConnection)
+            {
+                await connection.OpenAsync();
+                MySqlTransaction transaction = await connection.BeginTransactionAsync();
+                try
+                {
+                    TValue result = await transactionFunc(transaction);
+                    await transaction.CommitAsync();
+                    return result;
+                }
+                catch (MySqlException ex)
+                {
+                    await transaction.RollbackAsync();
+                    throw new Exception(ex.Message, ex);
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
         }
     }
 }
